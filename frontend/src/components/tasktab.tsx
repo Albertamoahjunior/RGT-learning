aee7c3b6aed315bbd28217893f617275a08c9d5b
Albertamoahjunior <albertamoah2000@gmail.com>
2024-10-24 12:23:12 +0000

    built the edit task feature with a minor issue

--- frontend/src/components/tasktab.tsx
+++ frontend/src/components/tasktab.tsx
@@ -2,12 +2,12 @@ import React, {useState} from 'react';
 import {Task} from '../models/task';
 import '../styles/tasktab.css'
 
-const TaskTab :React.FC<{task: Task; onDelete :(taskId : number) => void}>= (props) =>{
+const TaskTab :React.FC<{task: Task; onDelete :(taskId : number) => void; onEdit:(task: Task) => void}>= (props) =>{
   const [complete, setComplete] = useState<boolean>(false);
 
   return(
     <div className='task-tab'>
-      <button className='edit-button'> Edit </button>
+      <button onClick={() => props.onEdit(props.task)} className='edit-button'> Edit </button>
       <p>{props.task.title}</p>
       <div className='desc-area'>
         <p>{props.task.task}</p>
