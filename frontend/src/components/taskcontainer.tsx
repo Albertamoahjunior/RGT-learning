66a5a62e431d44075dbb4f76f7441399e6b8f98b
Albertamoahjunior <albertamoah2000@gmail.com>
2024-10-24 15:21:30 +0000

    editing feature

--- frontend/src/components/taskcontainer.tsx
+++ frontend/src/components/taskcontainer.tsx
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
+import EditTask from "./edittask";
 import NewTask from './newtask';
 import { Task } from '../models/task';
 import TaskTab from './tasktab';
 import '../styles/taskcontainer.css'
 
+//create a dummy task todo
+const dummy : Task = {
+  id: 1,
+  title: 'dummy',
+  task: 'dummy',
+  date: new Date().toLocaleDateString(),
+  complete: false
+}
+
 const TaskContainer: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [visibility, setVisibility] = useState<boolean>(false);
+  const [editVisibility, setEditVisibility] = useState<boolean>(false);
+  const [prevTask, setPrevTask] = useState<Task>(dummy);
 
   const handleAddTask = (newTask: Task) => {
     setTasks(prevTasks => [...prevTasks, newTask]); // Add the new task to the list
   };
+
+  const setUpEdit = (task: Task) =>{
+    setPrevTask(task);
+    setEditVisibility(true);
+  }
+
+  const handleEditTask = (task: Task) => {
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== task.id));
+
+    setTasks(prevTasks => [...prevTasks, task]);
+  }
 
   const deleteTask = (taskId: number) => {
   setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
@@ -20,8 +43,9 @@ const TaskContainer: React.FC = () => {
     <div className='task-container'>
       <h1>Task List</h1>
       <button onClick={()=> setVisibility(true)}>Add New Task</button>
-      {tasks.map(task => <TaskTab task={task} key={task.id} onDelete={deleteTask}/>)}
+      {tasks.map(task => <TaskTab task={task} key={task.id} onDelete={deleteTask} onEdit={setUpEdit}/>)}
       <NewTask isVisible={visibility} onAddTask={handleAddTask} setVisible={setVisibility}/>
+      <EditTask isVisible={editVisibility} onEditTask={handleEditTask} setVisible={setEditVisibility} prevTask={prevTask}/>
     </div>
   );
 }
