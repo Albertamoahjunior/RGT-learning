fde1201fd7e53f49096f7734846329ef1ae44a3a
Albertamoahjunior <albertamoah2000@gmail.com>
2024-10-25 10:34:23 +0000

    initial commit for the backend service but the controllers is acopy though and accurate

---
+++ backend/src/db.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import {Task} from './models';
+
+//read file and return content
+export async function general_read_file() :Promise<User[]>{
+  try {
+    let data = await fs.promises.readFile('tasks.json', 'utf8');
+    let tasks: Task[] = JSON.parse(data);
+    return tasks;
+  } catch (error) {
+    throw new Error('Failed to read file');
+  }
+}
+
+
+//write to file;
+export async function general_write_file(tasks :Task[]) :Promise<void> {
+  try {
+    await fs.promises.writeFile('tasks.json',  JSON.stringify(tasks), 'utf8');
+  } catch (error) {
+      throw new Error('failed to write');
+  }
+}
+
+//check if exists
+export function file_exist(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    fs.readFile('tasks.json', 'utf8', (err) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          resolve(false);  // File does not exist
+        } else {
+          reject(err);  // Handle other errors
+        }
+      } else {
+        resolve(true);  // File exists
+      }
+    });
+  });
+}
---
+++ backend/src/models.ts
@@ -0,0 +1,7 @@
+export interface Task{
+  id: number;
+  title: string;
+  task: string;
+  date: string;
+  complete: boolean;
+}
